2039e606bec657a1e25904e1033c83307bc987d8
chuncellor <cbschmidt32@gmail.com>
2023-08-24 16:07:07 -0400

    fixed start button, added scores.js, added basic css

--- assets/script.js
+++ assets/script.js
@@ -1,112 +1,105 @@
+var questionsEl = document.querySelector("#questions");
+var timerEl = document.querySelector("#time");
+var choicesEl = document.querySelector("#choices");
+var submitBtn = document.querySelector("#submit");
+var startBtn = document.querySelector("#start");
+var initialsEl = document.querySelector("#initials");
+var feedbackEl = document.querySelector("#feedback");
+
 var currentQuestionIndex = 0;
-var time = questions.length *15;
+var time = questions.length * 15;
 var timerId;
 
-var questionsEl = document.getElementById('questions' );
-var timerEl = document.getElementById('time');
-var choicesEl = document.getElementById('choices');
-var submitBtn =document.getElementById('submit');
-var startBtn = document.getElementById('start');
-var inititialsEl = document.getElementById('initials');
-var feedbackEl = document.getElementById('feedback');
-
-startBtn.addEventListener("click", startQuiz);
-
-
 function startQuiz() {
-    var startScreenEl = document.getElementById('start-screen');
-    startScreenEl.setAttribute('class', 'hide');
-    questionsEl.removeAtrribute('class');
-    timerId = setInterval(clockTick, 1000);
-    timerEl.textContent = time;
+  var startScreenEl = document.getElementById("start-screen");
+  startScreenEl.setAttribute("class", "hide");
+  questionsEl.removeAttribute("class");
+  timerId = setInterval(clockTick, 1000);
+  timerEl.textContent = time;
 
-
-    getQuestion ();
+  getQuestion();
 }
 
 function getQuestion() {
-    var currentQuestion = questions[currentQuestionIndex];
-    var titleEl = document.getElementById('question-title');
-    titleEl.textContent = currentQuestion.title;
-    choicesEl.innerHTML = " ";
-
-    for (var i = 0; i< currentQuestion.choices.length; i++) {
-        var choice = currentQuestion.choices[i];
-        var choiceNode =document.getElementById('button');
-        choiceNode.setAttribute('class', 'choice');
-        choiceNode.setAttribute('value', choice);
-        choiceNode.textContent = i + 1 + '. ' + choice;
-        choicesEl.appendChild(choiceNode);
-    }
+  var currentQuestion = questions[currentQuestionIndex];
+  var titleEl = document.getElementById("question-title");
+  titleEl.textContent = currentQuestion.title;
+  choicesEl.innerHTML = "";
+  currentQuestion.choices.forEach(function(choice, i) {
+    var choiceNode = document.createElement("button");
+    choiceNode.setAttribute("class", "choice");
+    choiceNode.setAttribute("value", choice);
+    choiceNode.textContent = i + 1 + ". " + choice;
+    choiceNode.onclick = questionClick;
+    choicesEl.appendChild(choiceNode);
+  });
 }
 
-function questionClick (event) {
-    var buttonEl = event.target
-    if (!buttonEl.matches('.choice')){
-        return;
+function questionClick() {
+  if (this.value !== questions[currentQuestionIndex].answer) {
+    time -= 15;
+    if (time < 0) {
+      time = 0;
     }
-    if (buttonEl.value !== questions[currentQuestionIndex].answer){
-        time -= 15;
-        if (time < 0){
-            time =0;
-        }
-
-        timerEl.textContent = time;
-        feedbackEl.textContent = 'Incorrect!';
-    } else {
-        feedbackEl.textContent = 'Correct!';
-    }
-
-    feedbackEl.setAttribute('class', 'feedback');
-    setTimeout (function(){
-        feedbackEl.setAttribute('class', 'feedback');
-    }, 1000);
-
-    currentQuestionIndex++;
-
-    if (time <= 0 || currentQuestionIndex === questions.length) {
-        quizEnd();
-    } else {
-        getQuestion();
-    }
+    timerEl.textContent = time;
+    feedbackEl.textContent = "Wrong!";
+    feedbackEl.style.color = "red";
+    feedbackEl.style.fontSize = "400%";
+  } else {
+    feedbackEl.textContent = "Correct!";
+    feedbackEl.style.color = "green";
+    feedbackEl.style.fontSize = "400%";
+  }
+  feedbackEl.setAttribute("class", "feedback");
+  setTimeout(function() {
+    feedbackEl.setAttribute("class", "feedback hide");
+  }, 1000);
+  currentQuestionIndex++;
+  if (currentQuestionIndex === questions.length) {
+    quizEnd();
+  } else {
+    getQuestion();
+  }
 }
 
 function quizEnd() {
-    clearInterval(timerId);
-    var endscreenEl =document.getElementById('end-screen');
-    endscreenEl.removeAttribute('class');
-    var finalscoreEl = document.getElementById('final-score');
-    finalscoreEl.textContent = time;
-    questionsEl.setAttribute('class', 'hide');
+  clearInterval(timerId);
+  var endScreenEl = document.getElementById("end-screen");
+  endScreenEl.removeAttribute("class");
+  var finalScoreEl = document.getElementById("final-score");
+  finalScoreEl.textContent = time;
+  questionsEl.setAttribute("class", "hide");
 }
 
 function clockTick() {
-    time --;
-    timerEl.textContent = time;
-    if (time <= 0) {
-        quizEnd();
-    }
+  time--;
+  timerEl.textContent = time;
+  if (time <= 0) {
+    quizEnd();
+  }
 }
 
-function saveHighscore () {
-    var initials =inititialsEl.value.trim();
-    if (initials !== ''){
-        var highscores =
-            JSON.parse(window.localStorage.getItem('highscores')) || [];
-        var newScore = {
-            score: time,
-            initials: initials,
-        };
+function saveHighscore() {
+  var initials = initialsEl.value.trim();
 
-        highscores.push (newScore);
-        window.localStorage.setItem('highscores', JSON.stringify(highscores));
-        window.location.href = 'highscores.html';
-    }
+  if (initials !== "") {
+    var highscores =
+      JSON.parse(window.localStorage.getItem("highscores")) || [];
+    var newScore = {
+      score: time,
+      initials: initials
+    };
+    highscores.push(newScore);
+    window.localStorage.setItem("highscores", JSON.stringify(highscores));
+    window.location.href = "score.html";
+  }
 }
 
 function checkForEnter(event) {
-    if (event.key === 'Enter') {
-        saveHighscore();
-    }
+  if (event.key === "Enter") {
+    saveHighscore();
+  }
 }
-
+submitBtn.onclick = saveHighscore;
+startBtn.onclick = startQuiz;
+initialsEl.onkeyup = checkForEnter;
